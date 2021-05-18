4dd921e2de3ceca555385b9c0b9657295c7ef4d8
Manfred Steyer <manfred.steyer@gmx.net>
2021-05-18 19:14:12 +0200

    adjust version settings in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -28,9 +28,9 @@ module.exports = {
       */
 
       shared: {
-        "@angular/core": { singleton: true, strictVersion: true },
-        "@angular/common": { singleton: true, strictVersion: true },
-        "@angular/router": { singleton: true, strictVersion: true }
+        "@angular/core": { },
+        "@angular/common": { },
+        "@angular/router": { }
       }
     }
 
