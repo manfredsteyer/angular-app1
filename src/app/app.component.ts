9dec0ff8ca6ab23c665dcb7b193f1c572b24f891
Manfred Steyer <manfred.steyer@gmx.net>
2021-05-18 16:04:51 +0200

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+declare const require: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+
+  ngVersion = require('../../package.json').dependencies['@angular/core'];
+
+  // constructor(private router: Router) { }
+
+  // ngOnInit(): void {
+  //   this.router.navigateByUrl(location.pathname.substr(1));
+  //   window.addEventListener('popstate', () => {
+  //     this.router.navigateByUrl(location.pathname.substr(1));
+  //   });
+  // }
+}
---
+++ src/app/app.module copy.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { endsWith } from './router.utils';
+import { AComponent } from './a/a.component';
+import { BComponent } from './b/b.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { matcher: endsWith('a'), component: AComponent},
+      { matcher: endsWith('b'), component: BComponent},
+    ])
+  ],
+  declarations: [
+    AComponent,
+    BComponent,
+    AppComponent
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule { 
+  constructor(private injector: Injector) {
+  }
+
+  ngDoBootstrap() {
+    const ce = createCustomElement(AppComponent, {injector: this.injector});
+    customElements.define('mfe2-element', ce);
+  }
+
+}
